b86350a00deb7c24b7938ff6be841f2530a253bc
trantiendat66 <dattt.ce190362@gmail.com>
2025-10-10 13:25:04 +0700

    add file src

---
+++ src/main/java/dao/ProductDAO.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import db.DBContext;
+import java.sql.*;
+import java.util.*;
+import model.Product;
+/**
+ *
+ * @author Tran Tien Dat - CE190362
+ */
+public class ProductDAO extends DBContext{
+    public List<Product> getAllProducts() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Product ORDER BY p.product_id DESC";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setProductId(rs.getInt("product_id"));
+                p.setCategoryId(rs.getInt("category_id"));
+                p.setAccountId(rs.getInt("account_id"));
+                p.setImage(rs.getString("image"));
+                p.setProductName(rs.getString("product_name"));
+                p.setPrice(rs.getInt("price"));
+                p.setBrand(rs.getString("brand"));
+                p.setOrigin(rs.getString("origin"));
+                p.setGender(rs.getBoolean("gender"));
+                p.setDescription(rs.getString("description"));
+                p.setWarranty(rs.getString("warranty"));
+                p.setMachine(rs.getString("machine"));
+                p.setGlass(rs.getString("glass"));
+                p.setDialDiameter(rs.getString("dial_diameter"));
+                p.setBezel(rs.getString("bezel"));
+                p.setStrap(rs.getString("strap"));
+                p.setDialColor(rs.getString("dial_color"));
+                p.setFunction(rs.getString("function"));
+                p.setQuantityProduct(rs.getInt("quantity_product"));
+                list.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public Product getProductById(int id) {
+        String sql = "SELECT * FROM Product p WHERE p.product_id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Product p = new Product();
+                p.setProductId(rs.getInt("product_id"));
+                p.setCategoryId(rs.getInt("category_id"));
+                p.setAccountId(rs.getInt("account_id"));
+                p.setImage(rs.getString("image"));
+                p.setProductName(rs.getString("product_name"));
+                p.setPrice(rs.getInt("price"));
+                p.setBrand(rs.getString("brand"));
+                p.setOrigin(rs.getString("origin"));
+                p.setGender(rs.getBoolean("gender"));
+                p.setDescription(rs.getString("description"));
+                p.setWarranty(rs.getString("warranty"));
+                p.setMachine(rs.getString("machine"));
+                p.setGlass(rs.getString("glass"));
+                p.setDialDiameter(rs.getString("dial_diameter"));
+                p.setBezel(rs.getString("bezel"));
+                p.setStrap(rs.getString("strap"));
+                p.setDialColor(rs.getString("dial_color"));
+                p.setFunction(rs.getString("function"));
+                p.setQuantityProduct(rs.getInt("quantity_product"));
+                return p;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Product p WHERE p.product_name LIKE ? ";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "%" + keyword + "%");
+            ps.setString(2, "%" + keyword + "%");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setProductId(rs.getInt("product_id"));
+                p.setCategoryId(rs.getInt("category_id"));
+                p.setAccountId(rs.getInt("account_id"));
+                p.setImage(rs.getString("image"));
+                p.setProductName(rs.getString("product_name"));
+                p.setPrice(rs.getInt("price"));
+                p.setBrand(rs.getString("brand"));
+                p.setOrigin(rs.getString("origin"));
+                p.setGender(rs.getBoolean("gender"));
+                p.setDescription(rs.getString("description"));
+                p.setWarranty(rs.getString("warranty"));
+                p.setMachine(rs.getString("machine"));
+                p.setGlass(rs.getString("glass"));
+                p.setDialDiameter(rs.getString("dial_diameter"));
+                p.setBezel(rs.getString("bezel"));
+                p.setStrap(rs.getString("strap"));
+                p.setDialColor(rs.getString("dial_color"));
+                p.setFunction(rs.getString("function"));
+                p.setQuantityProduct(rs.getInt("quantity_product"));
+                list.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
