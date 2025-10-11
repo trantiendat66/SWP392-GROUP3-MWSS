90de625f8563585a4cb1d5d55e2a6656b151d0aa
thuongckf1-cloud <thuongckf1@gmail.com>
2025-10-11 21:35:50 +0700

    Add files via upload

--- src/main/java/dao/CustomerDAO.java
+++ src/main/java/dao/CustomerDAO.java
@@ -23,9 +23,9 @@ public class CustomerDAO extends DBContext {
     // Lấy customer theo id
     public Customer getCustomerById(int id) {
         String sql = "SELECT Customer_id, Customer_name, Phone, Email, Address, Password, Dob, Gender, Account_status, Image FROM Customer WHERE Customer_id = ?";
-        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+        try ( PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, id);
-            try (ResultSet rs = ps.executeQuery()) {
+            try ( ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
                     Customer c = new Customer();
                     c.setCustomer_id(rs.getInt("Customer_id"));
@@ -57,7 +57,7 @@ public class CustomerDAO extends DBContext {
     // Cập nhật thông tin customer
     public boolean updateCustomer(Customer c) {
         String sql = "UPDATE Customer SET Customer_name = ?, Phone = ?, Email = ?, Address = ?, Password = ?, Dob = ?, Gender = ?, Image = ? WHERE Customer_id = ?";
-        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+        try ( PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, c.getCustomer_name());
             ps.setString(2, c.getPhone());
             ps.setString(3, c.getEmail());
@@ -80,5 +80,32 @@ public class CustomerDAO extends DBContext {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public Customer getCustomerByEmailAndPassword(String email, String password) {
+        String sql = "SELECT * FROM Customer WHERE email = ? AND password = ?";
+        try ( PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Customer c = new Customer();
+                c.setCustomer_id(rs.getInt("customer_id"));
+                c.setCustomer_name(rs.getString("customer_name"));
+                c.setEmail(rs.getString("email"));
+                c.setPhone(rs.getString("phone"));
+                c.setPassword(rs.getString("password"));
+                c.setGender(rs.getBoolean("gender") ? "Male" : "Female");
+                c.setDob(rs.getDate("dob"));
+                c.setAddress(rs.getString("address"));
+                c.setAccount_status(rs.getString("account_status"));
+                c.setImage(rs.getString("image"));
+                return c;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
