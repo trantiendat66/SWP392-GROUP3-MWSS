6acd6b2757ca823bd1745921c74c8912dfe14c09
DangViDanh <danhdvce190687@gmail.com>
2025-10-11 21:40:58 +0700

    Update Cart.java

--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -1,40 +1,46 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Document   : Cart Model
+ * Created on : Jan 10, 2025
+ * Author     : Dang Vi Danh - CE19687
  */
 package model;
 
-import java.time.LocalDateTime;
-
 /**
- *
- * @author Dang Vi Danh - CE190687
+ * Cart Model - Represents a cart item in the shopping cart
+ * @author Dang Vi Danh - CE19687
  */
 public class Cart {
-
     private int cartId;
-    private User user;
-    private Product product;
+    private int customerId;
+    private int productId;
+    private int price;
     private int quantity;
-    private LocalDateTime addDate;
-
+    
+    // Thông tin sản phẩm (join từ Product table)
+    private String productName;
+    private String productImage;
+    private String brand;
+    
     public Cart() {
     }
 
-    public Cart(int cartId, User user, Product product, int quantity, LocalDateTime addDate) {
+    public Cart(int cartId, int customerId, int productId, int price, int quantity) {
         this.cartId = cartId;
-        this.user = user;
-        this.product = product;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.price = price;
         this.quantity = quantity;
-        this.addDate = addDate;
     }
 
-    public Cart(User user, Product product, int quantity, LocalDateTime addDate) {
-        this.user = user;
-        this.product = product;
+    public Cart(int cartId, int customerId, int productId, int price, int quantity, String productName, String productImage, String brand) {
+        this.cartId = cartId;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.price = price;
         this.quantity = quantity;
-        this.addDate = addDate;
-        this.product = product;
+        this.productName = productName;
+        this.productImage = productImage;
+        this.brand = brand;
     }
 
     public int getCartId() {
@@ -45,20 +51,28 @@ public class Cart {
         this.cartId = cartId;
     }
 
-    public User getUser() {
-        return user;
+    public int getCustomerId() {
+        return customerId;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
     }
 
-    public Product getProduct() {
-        return product;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     public int getQuantity() {
@@ -69,16 +83,38 @@ public class Cart {
         this.quantity = quantity;
     }
 
-    public LocalDateTime getAddDate() {
-        return addDate;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setAddDate(LocalDateTime addDate) {
-        this.addDate = addDate;
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    
+    // Tính tổng tiền cho item này
+    public int getTotalPrice() {
+        return price * quantity;
     }
 
     @Override
     public String toString() {
-        return "Cart{" + "cartId=" + cartId + ", user=" + user + ", product=" + product + ", quantity=" + quantity + ", addDate=" + addDate + '}';
+        return "Cart{" + "cartId=" + cartId + ", customerId=" + customerId + ", productId=" + productId + ", price=" + price + ", quantity=" + quantity + ", productName=" + productName + ", productImage=" + productImage + ", brand=" + brand + '}';
     }
 }
+
