ec932e7a09983e0a934dae8efd4ab060c88d129e
Nguyen Thi Kieu Oanh <oanhntkce181400@fpt.edu.vn>
2025-10-12 12:30:54 +0700

    Add files via upload
    Upload registration code and forgot password no link, not nice interface

---
+++ utils/EmailUtil.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.ServletContext;
+import java.util.Properties;
+
+/**
+ *
+ * @author Oanh Nguyen
+ */
+/** Gửi email HTML qua SMTP, lấy cấu hình từ web.xml (context-param). */
+public class EmailUtil {
+    private final String host, port, user, pass, from;
+
+    public EmailUtil(ServletContext ctx) {
+        this.host = ctx.getInitParameter("mail.host");
+        this.port = ctx.getInitParameter("mail.port");
+        this.user = ctx.getInitParameter("mail.username");
+        this.pass = ctx.getInitParameter("mail.password");
+        this.from = ctx.getInitParameter("mail.from");
+    }
+
+    private Session buildSession() {
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        return Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+        });
+    }
+
+    public void send(String to, String subject, String html) throws MessagingException {
+        Session session = buildSession();
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(from));
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+        msg.setSubject(subject, "UTF-8");
+        msg.setContent(html, "text/html; charset=UTF-8");
+        Transport.send(msg);
+    }
+}
