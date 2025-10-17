fc303ff3d7f6410446017152239a80edbe1dd889
thuongckf1-cloud <thuongckf1@gmail.com>
2025-10-17 10:10:02 +0700

    Update StaffDAO.java

--- src/main/java/dao/StaffDAO.java
+++ src/main/java/dao/StaffDAO.java
@@ -1,6 +1,9 @@
 /*
+
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
  */
 package dao;
 
@@ -12,7 +15,10 @@ import model.Staff;
 
 /**
  *
+ *
+ *
  * @author Cola
+ *
  */
 public class StaffDAO extends DBContext {
 
@@ -24,15 +30,11 @@ public class StaffDAO extends DBContext {
         Staff staff = null;
         // Tên bảng Staff
         String sql = "SELECT * FROM Staff WHERE email = ?";
-
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
-
             ps.setString(1, email);
-
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
                     staff = new Staff();
-
                     staff.setAccountId(rs.getInt("account_id"));
                     staff.setUserName(rs.getString("user_name"));
                     staff.setEmail(rs.getString("email"));
@@ -42,18 +44,20 @@ public class StaffDAO extends DBContext {
                     staff.setPosition(rs.getString("position"));
                     staff.setAddress(rs.getString("address"));
                     staff.setStatus(rs.getString("status"));
-
                     if (!"Active".equalsIgnoreCase(staff.getStatus())) {
                         System.out.println("Staff account is not active: " + email);
                         return null;
                     }
                 }
             }
-
         } catch (SQLException e) {
             System.err.println("getStaffByEmail error: SQLState=" + e.getSQLState() + ", Code=" + e.getErrorCode());
             e.printStackTrace();
         }
         return staff;
+    }
+
+    public Staff getStaff(int accountId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
